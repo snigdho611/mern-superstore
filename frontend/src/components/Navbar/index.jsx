9b5762e4d44f69e1e24dec4c05f98fb5619aff66
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2022-05-16 22:58:17 +0600

    Updated Navbar

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import classes from './index.module.css'
 
 const Navbar = () => {
+    const navigate = useNavigate();
+
     return (
         <div className={classes.main}>
             <div className={classes.main_child}>
-                <div className={classes.navlink}>
+                <div className={classes.navlink} onClick={() => { navigate("/home") }}>
                     Home
                 </div>
-                <div className={classes.navlink}>
+                <div className={classes.navlink} onClick={() => { navigate("/products") }}>
                     Products
                 </div>
-                <div className={classes.navlink}>
+                <div className={classes.navlink} onClick={() => { navigate("/sellers") }}>
                     Best Sellers
                 </div>
-                <div className={classes.navlink}>
+                <div className={classes.navlink} onClick={() => { navigate("/deals") }}>
                     Deals
                 </div>
             </div>
