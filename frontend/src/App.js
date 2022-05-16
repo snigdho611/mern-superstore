641bdc20ffd35e2a9a562375d4c377fd6a8112ec
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2022-05-16 22:58:42 +0600

    Fixed the Routing section of the App.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
+import Home from "./components/Products";
+import Products from "./components/Products";
 
 const App = () => {
   // return (
@@ -34,6 +36,18 @@ const App = () => {
       <Routes>
         <Route index path="/" element={<Login />} />
         <Route index path="/home" element={<Navbar />} />
+        <Route
+          index
+          path="/products"
+          element={
+            <>
+              <Navbar />
+              <Products />
+            </>
+          }
+        />
+        <Route index path="/sellers" element={<Navbar />} />
+        <Route index path="/deals" element={<Navbar />} />
       </Routes>
     </BrowserRouter>
   );
