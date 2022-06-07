59fdc974277806475d0bfe8c87bb1c7d12f72ee3
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2022-06-07 12:40:28 +0600

    Revert "Added various validations for image uploads"
    This reverts commit 062a241ab937e5852f4cb6247aff39eb3cc4bbaa.

--- backend/controller/adminController.js
+++ backend/controller/adminController.js
@@ -2,31 +2,21 @@ const Product = require("../model/product");
 const { success, failure } = require("../utils/commonResponse");
 const HTTP_STATUS = require("../utils/httpStatus");
 const { validationResult } = require("express-validator");
-const fs = require("fs");
-const path = require("path");
 
 class productController {
   async addProduct(req, res, next) {
     try {
       const errors = validationResult(req);
-      console.log(req.file);
-      if (!req.file) {
-        errors.errors.push({
-          param: "productImage",
-          msg: "Product Image is required. Only jpeg, jpg and png file is allowed!",
-        });
-      }
       if (!errors.isEmpty()) {
         return res
           .status(HTTP_STATUS.UNPROCESSABLE_ENTITY)
-          .send(failure("Invalid inputs", errors.array()));
+          .send(failure("Invalid inputs", "Invalid inputs", errors.array()));
       }
-      console.log("image", req.file);
       const product = new Product({
         name: req.body.name,
         price: parseInt(req.body.price),
         weight: req.body.weight,
-        image: "files/" + req.file.filename,
+        image: req.body.image,
         description: req.body.description,
         type: req.body.type,
       });
@@ -49,37 +39,24 @@ class productController {
   async editProduct(req, res, next) {
     try {
       const errors = validationResult(req);
-      if (!req.file) {
-        errors.errors.push({
-          param: "productImage",
-          msg: "Product Image is required. Only jpeg, jpg and png file is allowed!",
-        });
-      }
       if (!errors.isEmpty()) {
         return res
           .status(HTTP_STATUS.UNPROCESSABLE_ENTITY)
           .send(failure("Invalid inputs", errors.array()));
       }
-      const target = await Product.findById(req.body.productId);
-      console.log(target);
-      const updateData = {
-        name: req.body.name,
-        price: parseInt(req.body.price),
-        weight: req.body.weight,
-        image: req.body.image,
-        description: req.body.description,
-        type: req.body.type,
-      };
-      if (req.file) {
-        fs.unlink(path.join(__dirname, "..", target.image), function () {
-          console.log("Deleted existing image link");
-        });
-      }
-      updateData.image = "files/" + req.file.filename;
-
-      const result = await Product.findOneAndUpdate({ _id: req.body.productId }, updateData, {
-        new: false,
-      }).exec();
+      //   console.log(req.body.productId);
+      const result = await Product.findOneAndUpdate(
+        { _id: req.body.productId },
+        {
+          name: req.body.name,
+          price: parseInt(req.body.price),
+          weight: req.body.weight,
+          image: req.body.image,
+          description: req.body.description,
+          type: req.body.type,
+        },
+        { new: false }
+      ).exec();
       //   console.log(result);
       if (result) {
         return res
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -2,65 +2,9 @@ const express = require("express");
 const router = express.Router();
 const adminController = require("../controller/adminController");
 const validator = require("../middleware/validation");
-const multer = require("multer");
-const path = require("path");
 
-const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    if (file) {
-      cb(null, "files");
-    } else {
-      cb("No file found", null);
-    }
-  },
-  filename: (req, file, cb) => {
-    if (file) {
-      cb(
-        null,
-        file.originalname.split(".")[0].replace(/\ /g, "") +
-          Date.now() +
-          path.extname(file.originalname)
-      );
-    } else {
-      cb("No file found", null);
-    }
-  },
-});
-
-const checkImage = (req, file, cb) => {
-  if (file) {
-    if (
-      file.mimetype === "image/jpeg" ||
-      file.mimetype === "image/jpg" ||
-      file.mimetype === "image/png"
-    ) {
-      cb(null, true);
-    } else {
-      cb(null, false);
-    }
-  } else {
-    cb("No file found", false);
-  }
-};
-
-const upload = multer({
-  storage: fileStorage,
-  limits: 30000,
-  fileFilter: checkImage,
-});
-
-router.post(
-  "/products/add",
-  upload.single("productImage"),
-  validator.addProduct,
-  adminController.addProduct
-);
-router.put(
-  "/products/edit",
-  upload.single("productImage"),
-  validator.updateProduct,
-  adminController.editProduct
-);
+router.post("/products/add", validator.addProduct, adminController.addProduct);
+router.put("/products/edit", validator.updateProduct, adminController.editProduct);
 router.delete("/products/delete", validator.deleteProduct, adminController.deleteProduct);
 
 module.exports = router;
