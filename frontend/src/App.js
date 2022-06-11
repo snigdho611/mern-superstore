7f7e4f13a9a0cdd9e3e840e2edbffba77f49456d
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2022-06-11 08:19:11 +0600

    Updates to routes and pages
    - Added routes and page for Signup form
    - Created a simple frontend for email verification

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,108 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LoginPage from "pages/LoginPage";
-import HomePage from "pages/HomePage";
-import ProductsPage from "pages/ProductsPage";
-import ProductsIdPage from "pages/ProductsIdPage";
-import DealsPage from "pages/Deals";
-import ChangePage from "pages/ChangePage";
-import CartPage from "pages/CartPage";
+// import LoginPage from "pages/LoginPage";
+// import HomePage from "pages/HomePage";
+// import ProductsPage from "pages/ProductsPage";
+// import ProductsIdPage from "pages/ProductsIdPage";
+// import DealsPage from "pages/Deals";
+// import ChangePage from "pages/ChangePage";
+// import CartPage from "pages/CartPage";
 import Header from "components/Header";
 import Navbar from "components/Navbar";
 import Footer from "components/Footer";
 import ChangeProduct from "components/ChangeProduct";
 import AddProduct from "components/AddProduct";
 import VerifyEmail from "components/VerifyEmail";
+import Register from "components/Authenticate/Register";
+import Cart from "components/Cart";
+import Form from "components/Form";
+import Deals from "components/Deals";
+import Home from "components/Home";
+import Login from "components/Authenticate/Login";
+import ProductDetails from "components/ProductDetails";
+import ProductList from "components/ProductList";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/products/:productId" element={<ProductsIdPage />} />
-        <Route path="/deals" element={<DealsPage />} />
-        <Route path="/change" element={<ChangePage />} />
-        <Route path="/cart" element={<CartPage />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Login />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <>
+              <Header />
+              <Navbar />
+              <Home />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/products"
+          element={
+            <>
+              <Header />
+              <Navbar />
+              {/* <ProductDetails /> */}
+              <ProductList />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/products/:productId"
+          element={
+            <>
+              <Header />
+              <Navbar />
+              <ProductDetails />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/deals"
+          element={
+            <>
+              <Header />
+              <Navbar />
+              <Deals />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/change"
+          element={
+            <>
+              <Header />
+              <Navbar />
+              <Form />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <>
+              <Header />
+              <Navbar />
+              <Cart />
+              <Footer />
+            </>
+          }
+        />
         <Route
           path="/products/edit/:productId"
           element={
@@ -43,6 +121,18 @@ const App = () => {
               <Header />
               <Navbar />
               <AddProduct />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <>
+              <Header />
+              {/* <Navbar /> */}
+              {/* <AddProduct /> */}
+              <Register />
               <Footer />
             </>
           }
