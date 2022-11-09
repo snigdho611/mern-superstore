eacb2763827b0cad51c6c6d8927416ab3f0398bb
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2022-11-10 02:22:12 +0600

    Updated pagination on frontend

--- frontend/src/pages/ProductsPage/index.tsx
+++ frontend/src/pages/ProductsPage/index.tsx
@@ -6,25 +6,55 @@ import Navbar from "components/Navbar";
 // import ProductList from "components/ProductList";
 import ProductCard from "components/ProductCard";
 import React, { useEffect, useState } from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
 import { Product } from "types";
 import "./index.css";
 
 const ProductsPage = () => {
-  const [originalData, setOriginalData] = useState<Product[]>([]);
+  // const [originalData, setOriginalData] = useState<Product[]>([]);
   const [dataToShow, setDataToShow] = useState<Product[]>([]);
+  const [total, setTotal] = useState<number>(0);
   const [response, setResponse] = useState<Response>({
     success: false,
     loading: false,
     message: "",
   });
+
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  // console.log(searchParams.get("text"));
+
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_BASE_BACKEND}/products/all`, {
+    // console.log(searchParams.get("page"));
+    fetch(
+      `${process.env.REACT_APP_BASE_BACKEND}/products/all?page=${searchParams.get("page")}&limit=8`,
+      { method: "GET" }
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        // console.log(json.results.products);
+        // setOriginalData(json.results.products);
+        if (json.results.products.length) {
+          setDataToShow(json.results.products);
+          setResponse((prevState) => ({ ...prevState, loading: false }));
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [searchParams]);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_BASE_BACKEND}/products/all?page=1&limit=8`, {
       method: "GET",
     })
       .then((response) => response.json())
       .then((json) => {
         // console.log(json.results.products);
-        setOriginalData(json.results.products);
+        // setOriginalData(json.results.products);
+        console.log(json.results);
+        setTotal(json.results.total);
+        setDataToShow(json.results.products);
         setResponse((prevState) => ({ ...prevState, loading: false }));
       })
       .catch((err) => {
@@ -32,14 +62,15 @@ const ProductsPage = () => {
       });
   }, []);
 
-  useEffect(() => {
-    setDataToShow(originalData);
-  }, [originalData]);
+  // useEffect(() => {
+  //   setDataToShow(originalData);
+  // }, [originalData]);
 
   useEffect(() => {
     console.log(dataToShow.length);
   }, [dataToShow]);
 
+  // console.log(Array(8).fill(1));
   return (
     <>
       <Header />
@@ -65,6 +96,32 @@ const ProductsPage = () => {
         ) : (
           <div>No results</div>
         )}
+      </div>
+      <div
+        style={{
+          margin: "0 auto",
+          width: "50%",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        {/* {console.log(Array(originalData.length / 8))} */}
+        {Array.from(Array(Math.ceil(total / 8)).keys()).map((element) => {
+          return (
+            <button
+              onClick={() => {
+                navigate({
+                  pathname: "/products",
+                  search: `?page=${element + 1}`,
+                });
+              }}
+              style={{ width: "80px", outline: "#bbb solid 1px" }}
+            >
+              {element + 1}
+            </button>
+          );
+        })}
       </div>
       <Footer />
     </>
