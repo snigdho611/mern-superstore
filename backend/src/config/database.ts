1c057cb0b4c8cef987b594fb8e43f23ec20feed3
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2023-01-14 15:21:26 +0600

    Removal of unnecessary packages
    - bcrypt from backend
    - tailwindcss from frontend

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -4,14 +4,16 @@ import mongoose from "mongoose";
 const databaseConnection = async (callback: Application["listen"]) => {
   try {
     console.log("Connecting to database...");
-    // console.log(process.env.DATABASE_URL);
-    if (process.env.LOCAL_DATABASE_URL) {
+    console.log(callback);
+    if (process.env.DATABASE_URL) {
       // const client = await mongoose.connect(process.env.DATABASE_URL);
-      const client = await mongoose.connect(process.env.LOCAL_DATABASE_URL);
+      const client = await mongoose.connect(process.env.DATABASE_URL);
       if (client) {
         console.log("Database connection completed");
       }
       callback();
+    } else {
+      console.log("Database connection string unavailable");
     }
   } catch (error: any) {
     console.log(error);
