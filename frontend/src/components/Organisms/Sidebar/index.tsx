9a453437aed90e827180f62300aa2a5cd36c95c6
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2023-04-27 00:47:08 +0600

    Updated sidebar functionalities

--- frontend/src/components/Organisms/Sidebar/index.tsx
+++ frontend/src/components/Organisms/Sidebar/index.tsx
@@ -1,10 +1,41 @@
 import Accordion from "components/Molecules/Accordion";
 import React from "react";
 
+const productsList = [
+  {
+    id: 1,
+    category: "sound & audio",
+    list: [
+      {
+        id: 1,
+        name: "headphone",
+      },
+      {
+        id: 1,
+        name: "earbud",
+      },
+    ],
+  },
+  {
+    id: 2,
+    category: "laptops",
+    list: [
+      {
+        id: 1,
+        name: "laptop macbook",
+      },
+      {
+        id: 2,
+        name: "laptop lenovo",
+      },
+    ],
+  },
+];
+
 const Sidebar = () => {
   return (
     <div>
-      <Accordion />
+      <Accordion data={productsList} />
     </div>
   );
 };
