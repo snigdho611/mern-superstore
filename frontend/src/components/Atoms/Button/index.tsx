73fda16fd8e0d9f65c9d393351dc546dd6cba56a
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2023-04-27 00:46:54 +0600

    Accordion created

---
+++ frontend/src/components/Atoms/Button/index.tsx
@@ -0,0 +1,2 @@
+const Button = () => {
+  return <div>Button</div>;
--- frontend/src/components/Molecules/Accordion/index.tsx
+++ frontend/src/components/Molecules/Accordion/index.tsx
@@ -1,10 +1,37 @@
-import NavButton from "components/Atoms/NavButton";
-import React from "react";
+import React, { useState } from "react";
 
-const Accordion = () => {
+interface IAccordion {
+  data: Array<{ id: number; category: string; list: Array<{ id: number; name: string }> }>;
+}
+
+const Accordion: React.FC<IAccordion> = ({ data }) => {
+  const [open, setOpen] = useState<{ id: number | null; state: boolean }>({
+    id: null,
+    state: false,
+  });
   return (
-    <div>
-      <NavButton />
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      {data.map(({ id, category, list }) => {
+        return (
+          <>
+            <button
+              key={id}
+              onClick={() => {
+                setOpen({ id: id, state: !open.state });
+              }}
+            >
+              {category}
+            </button>
+            {open.id === id && open.state ? (
+              <div>
+                {list.map(({ id, name }) => {
+                  return <div key={id}>{name}</div>;
+                })}
+              </div>
+            ) : null}
+          </>
+        );
+      })}
     </div>
   );
 };
